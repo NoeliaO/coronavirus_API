3dc2f4ef13a9b7d9b0ce7ad4c66e44124d887557
NoeliaO <noeliaotaro@gmail.com>
2020-05-07 11:50:40 +0100

    nw

--- coronavirus_api_noelia_otazo.py
+++ coronavirus_api_noelia_otazo.py
@@ -6,15 +6,34 @@ import os
 
 #La dirección de la API es: https://api.covid19api.com/
 try:        
-    def 
+    def sumario():
+        api_address="https://api.covid19api.com/summary"
+        resp = requests.get(api_address)
+        if resp.status_code==200:
+            json_data=json.loads(resp.content)
+            print(type(json_data))
+            print(json_data)
+
+        input("Presione una tecla para continuar...")    
+
+    def datos_pais():
+        api_address="https://api.covid19api.com/countries"
+        resp = requests.get(api_address)
+        if resp.status_code==200:
+            json_data=json.loads(resp.content)
+            print(type(json_data))
+            print(json_data)
+
+        input("Presione una tecla para continuar...")  
+
 
 
     while True:
         #borrar la pantalla Para DOS/Windows
         os.system ("cls") 
-        print("Gestión de datos de municipios de Castilla y León")
-        print("1. ")
-        print("2. Busqueda de municipios mediante provincia.")
+        print("Covid-19")
+        print("1. Sumario de casos actualizado diariamente.")
+        print("2. Datos por País.")
         print("3. Busqueda de provincia y población mediante municipio.")
         print("4. Busqueda de municipios mediante Mancomunidad.")
         print("5. Enumeración de municipios mediante provincia.")
@@ -24,13 +43,17 @@ try:
         opcion = int(input())
         
         if opcion==1:
-            print("bip bip")
+            sumario()
+
         elif opcion==2:
-            print("bip bip")
+            datos_pais()1
+
         elif opcion==3:
             print("bip bip")
+
         elif opcion==4:
             print("bip bip")
+
         elif opcion==5:
             break
  
